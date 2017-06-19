e2e4844a543e9c076e82c9534d29d1780aacac75
igorfel <appsigor@gmail.com>
2017-06-19 02:18:03 -0300

    RMI updates
    GUI implemented for client-side

--- RMIClient/src/rmiclient/RMIClient.java
+++ RMIClient/src/rmiclient/RMIClient.java
@@ -6,7 +6,6 @@
 package rmiclient;
 
 import RMIInterfaces.RMIInterfaces;
-import static java.lang.Thread.sleep;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -16,20 +15,16 @@ import java.rmi.registry.Registry;
  *
  * @author igorf
  */
-public class RMIClient  {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws RemoteException, InterruptedException, NotBoundException {
-        Registry regclient = LocateRegistry.getRegistry("localhost", 5555);
-        
-        RMIInterfaces rmiRequest = (RMIInterfaces) regclient.lookup("rmiServer");
-        for (int i = 0; i <= 10; i++) {
-            sleep(300);
-            String msg = rmiRequest.getMessage();
-            System.out.println(msg + ":" + i);
-        }
+public class RMIClient {
+    
+    RMIInterfaces rmiRequest;
+    
+    public void connect2Server() throws RemoteException, NotBoundException {
+       Registry regclient = LocateRegistry.getRegistry("localhost", 5555);
+       rmiRequest = (RMIInterfaces) regclient.lookup("rmiServer");
     }
     
+    public String getRMIMessage() throws RemoteException{
+        return rmiRequest.getMessage();
+    }
 }
